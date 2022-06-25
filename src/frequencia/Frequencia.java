698e1ca31ee859f47f77bb01b433207a55e310e9
Davi Ximenes <davixmnsl@gmail.com>
2022-06-25 17:55:42 -0300

    Conclusão

--- src/frequencia/Frequencia.java
+++ src/frequencia/Frequencia.java
@@ -2,20 +2,21 @@ package frequencia;
 
 public class Frequencia {
     private final int[] frequenciaVetor;
+    private final int utf8 = 255;
 
     public Frequencia() {
-        this.frequenciaVetor = new int[255];
+        this.frequenciaVetor = new int[utf8];
     }
 
     public void incrementar(int decimal) {
-        if(decimal > 255)
+        if(decimal > this.utf8)
             throw new ArrayIndexOutOfBoundsException("Índice inválido!!!");
 
         this.frequenciaVetor[decimal] += 1;
     }
 
     public int getFrequencia(int decimal) {
-        if(decimal > 255)
+        if(decimal > this.utf8)
             throw new ArrayIndexOutOfBoundsException("Índice inválido!!!");
 
         return this.frequenciaVetor[decimal];
